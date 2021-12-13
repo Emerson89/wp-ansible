bab4125c1e787825395d531fb8e45585061720a9
emerson <emerson.cesario50@gmail.com>
2021-12-13 10:20:32 -0300

    commit jenkins

--- build_centos.sh
+++ build_centos.sh
@@ -5,7 +5,7 @@ DOCKER_IMAGE="emr001/centos7-emr"
 INIT=/usr/lib/systemd/systemd
 RUN_OPTS="--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"  
 
-docker run -ti $RUN_OPTS --detach --volume="${PWD}":/etc/ansible/roles/role_under_test:ro -p 8080:80 --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE $INIT
+docker run -ti $RUN_OPTS --detach --volume="${PWD}":/etc/ansible/roles/role_under_test:ro -p 8282:80 --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE $INIT
 
 docker exec $DOCKER_CONTAINER_NAME ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml
 
